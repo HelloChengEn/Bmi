67d9140c4f858e4a0a7a05ab81b69bb7484bd912
ChengEn <smiletsai871017@gmail.com>
2022-07-02 18:05:59 +0800

    linear layout

--- app/src/main/java/com/tom/bmi/MainActivity.kt
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.tom.bmi.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        b_help.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("BMI說明")
+                .setMessage("體重(kg)/身高的平方(m)")
+                .setPositiveButton("OK",null)
+                .show()
+        }
     }
 
     fun bmi(view: View){
@@ -22,5 +31,13 @@ class MainActivity : AppCompatActivity() {
         val height = ed_height.text.toString().toFloat()
         val bmi = weight / (height * height)
         Log.d("BMI", bmi.toString())
+        Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle("Your BMI")
+            .setPositiveButton("OK", null)
+            .setNeutralButton("Cancel", null)
+            .show()
     }
+
 }
